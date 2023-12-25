75cf15a7a4dd9dfcc34f08f7eaa19ddc449b9213
Muhammad Nizar <mnizarzr@gmail.com>
2023-12-25 20:51:30 +0700

    add migration

---
+++ database/migrations/2023_12_25_115142_create_needed_tables.php
@@ -0,0 +1,73 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->unsignedInteger('capacity')->default(50);
+            $table->string('location');
+            $table->point('location_coords')->nullable();
+            $table->dateTime('held_date');
+            $table->timestamps();
+        });
+
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedDecimal('amount');
+            $table->string('status')->default('pending');
+            $table->string('payment_channel');
+            $table->string('payment_link', 512);
+            $table->timestamp('expire_at');
+
+            $table->timestamps();
+        });
+
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+
+            $table->foreignId('transaction_id')->nullable()
+                ->references('id')->on('transactions')
+                ->onDelete('set null')->onUpdate('cascade');
+
+            $table->foreignId('event_id')->nullable()
+                ->references('id')->on('events')
+                ->onDelete('set null')->onUpdate('cascade');
+
+            $table->foreignId('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade')->onUpdate('cascade');
+
+            $table->string('holder_name');
+            $table->enum('holder_gender', ['male', 'female'])->default('male');
+            $table->string('holder_email');
+            $table->string('holder_phone')->nullable();
+            $table->unsignedDecimal('purchase_amount');
+            $table->string('notes')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+        Schema::dropIfExists('transactions');
+        Schema::dropIfExists('tickets');
+    }
+};
